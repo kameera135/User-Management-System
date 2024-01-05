48196765f4e4860af7900e04dfe3a6cdf1be1d60
Thushara Makuloluwa <139602736+ThusharaMakuloluwa@users.noreply.github.com>
2024-01-05 16:06:17 +0530

    Password Policy -> UI

--- src/app/pages/modules/cams-new/password-policy/password-policy.component.ts
+++ src/app/pages/modules/cams-new/password-policy/password-policy.component.ts
@@ -1,13 +1,8 @@
 import { Component } from "@angular/core";
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { AppService } from "src/app/app.service";
 import { MessageService } from "src/app/services/PopupMessages/message.service";
 import { BreadcrumbService } from "src/app/services/breadcrumb/breadcrumb.service";
-import { ActivityLogsService } from "src/app/services/cams-new/activity-logs.service";
 import { PasswordPolicyService } from "src/app/services/cams-new/password-policy.service";
-import { ActivityLogData } from "src/app/shared/models/Cams-new/ActivityLogData";
-import { tableOptions } from "src/app/shared/models/tableOptions";
-import { ActivityLogsModalComponent } from "src/app/shared/widget/config/activity-logs-modal/activity-logs-modal.component";
 
 @Component({
   selector: "app-password-policy",
@@ -15,353 +10,113 @@ import { ActivityLogsModalComponent } from "src/app/shared/widget/config/activit
   styleUrls: ["./password-policy.component.scss"],
 })
 export class PasswordPolicyComponent {
-  loadingInProgress: boolean = false;
-
   passwordMinLength!: number;
   passwordMaxLength!: number;
+  passwordMinimumValidity!: number;
+  passwordMinimumValidityTimeUnit!: string;
+  passwordExpiry!: number;
+  passwordExpiryTimeUnit!: string;
+  passwordRepeatCheck!: number;
+  maxAttempts!: number;
+  lockoutDuration!: number;
+  lockoutDurationTimeUnit!: string;
+  retryAttempts!: number;
 
-  selectedYear: string = new Date().getFullYear().toString();
-  selectedMonth: string = (new Date().getMonth() + 1).toString();
-  yearList: any[] = [];
-  monthList: any[] = [];
+  canChangePasswordLengthMin: boolean = false;
+  canChangePasswordLengthMax: boolean = false;
+  canChangePasswordMinimumValidity: boolean = false;
+  canChangePasswordExpiry: boolean = false;
+  passwordExpiryWarning: boolean = false;
+  canChangePasswordRepeatCheck: boolean = false;
+  canChangeAccountLockout: boolean = false;
+  isAbleLetters: boolean = false;
+  isAbleNumericCharacters: boolean = false;
+  isAbleSpecialCharacters: boolean = false;
+  isAbleMixedCaseLetters: boolean = false;
 
-  userModel!: ActivityLogData;
-  userList!: ActivityLogData[];
-  userDetailsArray: any = [];
+  passwordMinLengthDefault: number = 1;
+  passwordMaxLengthDefault: number = 50;
 
-  totalDataCount!: number;
-  selectedPage: number = 1;
-  selectedPageSize: number = 20;
-
-  searchTerm!: string;
-
-  roleList: any[] = [{ value: "All Roles", id: 1 }];
-  selectedRole: string = this.roleList[0].value;
-  platformList: any[] = [{ value: "All Platforms", id: 1 }];
-  selectedPlatform: string = this.platformList[0].value;
-
-  UserUpdatedNotificationMessage!: string;
-
-  serchedTerm!: string;
-
-  usersViewTableOptions: tableOptions = new tableOptions();
-
-  headArray = [
-    { Head: "ID", FieldName: "ID", ColumnType: "Data" },
-    { Head: "User Name", FieldName: "UserName", ColumnType: "Data" },
-    { Head: "Platform", FieldName: "Platform", ColumnType: "Data" },
-    { Head: "Role", FieldName: "Role", ColumnType: "Data" },
-    { Head: "Activity Type", FieldName: "ActivityType", ColumnType: "Data" },
-    { Head: "Description", FieldName: "Description", ColumnType: "Data" },
-    { Head: "Time", FieldName: "Time", ColumnType: "Data" },
-    { Head: "Actions", FieldName: "", ColumnType: "Action" },
-  ];
-
-  //to remove
-  tableData = [
-    {
-      ID: "1254123658",
-      UserName: "Harper.Bennett",
-      Platform: "TBS",
-      Role: "Tenant",
-      ActivityType: "SingleSignOn-Auth",
-      Description: "Logged into the platform",
-      Time: "Dec 19, 2023, 9:15:54 PM",
-      Details: {
-        Setting: "PS_PAGE",
-        Data: 20,
-        Description: "Pagination Page Size",
-      },
-    },
-    {
-      ID: "4512589562",
-      UserName: "Mia.Rodriguez",
-      Platform: "TBS",
-      Role: "Tenant",
-      ActivityType: "SingleSignOn-Auth",
-      Description: "Logged into the platform",
-      Time: "Dec 19, 2023, 9:05:50 PM",
-      Details: "userId = 2218",
-    },
-    {
-      ID: "7895624568",
-      UserName: "Nolan.Sullivan",
-      Platform: "TBS",
-      Role: "Tenant",
-      ActivityType: "SingleSignOn-Auth",
-      Description: "Logged into the platform",
-      Time: "Dec 19, 2023, 19:13:43 PM",
-      Details: [
-        {
-          Date: "2023-10-22T00:00:00",
-          NormalConsumption: 27.745999384,
-          ExtendedConsumption: 25.045599445,
-        },
-        {
-          Date: "2023-10-21T00:00:00",
-          NormalConsumption: 27.777549384,
-          ExtendedConsumption: 25.000199445,
-        },
-        {
-          Date: "2023-10-12T00:00:00",
-          NormalConsumption: 27.725999384,
-          ExtendedConsumption: 29.000199445,
-        },
-      ],
-    },
+  timeUnitsList = [
+    { id: "SC", value: "Seconds" },
+    { id: "MI", value: "Minutes" },
+    { id: "HO", value: "Hours" },
+    { id: "DY", value: "Days" },
+    { id: "WK", value: "Weeks" },
+    { id: "YR", value: "Years" },
   ];
 
   constructor(
     private breadcrumbService: BreadcrumbService,
     private shared: PasswordPolicyService,
-    private modalService: NgbModal,
     private appService: AppService,
     private alertService: MessageService
   ) {}
 
   ngOnInit(): void {
-    this.passwordMinLength = 8;
-    this.passwordMaxLength = 16;
-
-    const currentYear = new Date().getFullYear();
-    this.yearList = Array.from(
-      { length: this.appService.appConfig[0].maximumYearRange },
-      (_, index) => {
-        const year = currentYear - index;
-        return { value: year.toString(), id: index + 1 };
-      }
-    );
-
-    this.monthList = this.appService.appConfig[0].months;
-
-    var roles = this.appService.appConfig[0].roleList;
-    for (let i = 0; i < roles.length; i++) {
-      this.roleList.push(roles[i]);
-    }
-
-    //to edit
-    var platforms = [
-      { value: "AES", id: 2 },
-      { value: "TBS", id: 3 },
-      { value: "EMS", id: 4 },
-    ];
-    for (let i = 0; i < platforms.length; i++) {
-      this.platformList.push(platforms[i]);
-    }
-
-    this.usersViewTableOptions.allowViewActionsButton = true;
-
     this.breadcrumbService.loadBreadcrumbValue([
-      { label: "Activity Logs", active: false },
-      { label: "Activity Logs", active: true },
+      { label: "Password Policy", active: false },
+      { label: "Password Policy", active: true },
     ]);
   }
 
-  loadData() {
-    this.loadingInProgress = true;
-    // if (
-    //   (this.serchedTerm == undefined ||
-    //     this.serchedTerm == null ||
-    //     this.serchedTerm == "") &&
-    //   (this.selectedRole == undefined ||
-    //     this.selectedRole == null ||
-    //     this.selectedRole == "All" ||
-    //     this.selectedRole == "")
-    // ) {
-    //   this.getAllUsers();
-    // } else if (
-    //   (this.serchedTerm != undefined ||
-    //     this.serchedTerm != null ||
-    //     this.serchedTerm != "") &&
-    //   (this.selectedRole == undefined ||
-    //     this.selectedRole == null ||
-    //     this.selectedRole == "All" ||
-    //     this.selectedRole == "")
-    // ) {
-    //   this.searchUsers(this.serchedTerm);
-    // } else if (
-    //   (this.serchedTerm == undefined ||
-    //     this.serchedTerm == null ||
-    //     this.serchedTerm == "") &&
-    //   (this.selectedRole != undefined ||
-    //     this.selectedRole != null ||
-    //     this.selectedRole != "All" ||
-    //     this.selectedRole != "")
-    // ) {
-    //   this.getUsersByRole(this.serchedTerm);
-    // } else if (
-    //   (this.serchedTerm != undefined ||
-    //     this.serchedTerm != null ||
-    //     this.serchedTerm != "") &&
-    //   (this.selectedRole != undefined ||
-    //     this.selectedRole != null ||
-    //     this.selectedRole != "All" ||
-    //     this.selectedRole != "")
-    // ) {
-    //   this.searchUsersByRole(this.serchedTerm, this.selectedRole);
-    // } else {
-    //   this.getAllUsers();
-    //   this.alertService.sideErrorAlert("Error", "Could not retrive data");
-    // }
+  toggleChangePasswordLengthMin() {
+    this.canChangePasswordLengthMin = !this.canChangePasswordLengthMin;
   }
 
-  updateTable() {
-    this.userDetailsArray = this.userList.map((item) => ({
-      UserName: item.username,
-      Role: item.role,
-    }));
-    this.tableData = this.userDetailsArray;
+  toggleChangePasswordLengthMax() {
+    this.canChangePasswordLengthMax = !this.canChangePasswordLengthMax;
   }
 
-  onPaginationChange(page: number): void {
-    this.selectedPage = page;
-    this.loadData();
-  }
-  onPagesizeChange(pageSize: number): void {
-    this.selectedPageSize = pageSize;
-    this.loadData();
+  toggleChangePasswordMinimumValidity() {
+    this.canChangePasswordMinimumValidity =
+      !this.canChangePasswordMinimumValidity;
   }
 
-  onViewButtonClicked(row: any) {
-    this.openModal("View", "Extra Details", row.Details);
+  toggleChangePasswordExpiry() {
+    this.canChangePasswordExpiry = !this.canChangePasswordExpiry;
   }
 
-  openModal(type: string, modalTitle: string, details: any): void {
-    const modalRef = this.modalService.open(ActivityLogsModalComponent, {
-      size: "s",
-      centered: true,
-      backdrop: "static",
-      keyboard: false,
-    });
-    modalRef.componentInstance.modalTitle = modalTitle;
-    modalRef.componentInstance.details = details;
+  toggleChangePasswordExpiryWarning() {
+    this.passwordExpiryWarning = !this.passwordExpiryWarning;
   }
 
-  // getAllUsers() {
-  //   this.shared
-  //     .getAllUsers(this.selectedPage, this.selectedPageSize)
-  //     .subscribe({
-  //       next: (response) => {
-  //         this.userList = response.response;
-  //         this.totalDataCount = response.rowCount;
-  //         this.updateTable();
-  //         this.loadingInProgress = false;
-  //       },
-  //       error: (error) => {
-  //         this.alertService.sideErrorAlert(
-  //           "Error",
-  //           this.appService.popUpMessageConfig[0]
-  //             .GetUserListSuccessSideAlertMessage
-  //         );
+  toggleChangePasswordRepeatCheck() {
+    this.canChangePasswordRepeatCheck = !this.canChangePasswordRepeatCheck;
+  }
 
-  //         this.userList = [];
-  //         this.totalDataCount = 0;
+  toggleChangeAccountLockout() {
+    this.canChangeAccountLockout = !this.canChangeAccountLockout;
+  }
 
-  //         this.updateTable();
-  //         this.loadingInProgress = false;
-  //       },
-  //     });
-  // }
+  toggleLetters() {
+    this.isAbleLetters = !this.isAbleLetters;
+  }
 
-  // getUsersByRole(role: string) {
-  //   this.shared
-  //     .getUsersByRole(role, this.selectedPage, this.selectedPageSize)
-  //     .subscribe({
-  //       next: (response) => {
-  //         this.userList = response.response;
-  //         this.totalDataCount = response.rowCount;
-  //         this.updateTable();
-  //         this.loadingInProgress = false;
-  //       },
-  //       error: (error) => {
-  //         this.alertService.sideErrorAlert(
-  //           "Error",
-  //           this.appService.popUpMessageConfig[0]
-  //             .GetUserListSuccessSideAlertMessage
-  //         );
+  toggleNumericCharacters() {
+    this.isAbleNumericCharacters = !this.isAbleNumericCharacters;
+  }
 
-  //         this.userList = [];
-  //         this.totalDataCount = 0;
+  toggleSpecialCharacters() {
+    this.isAbleSpecialCharacters = !this.isAbleSpecialCharacters;
+  }
 
-  //         this.updateTable();
-  //         this.loadingInProgress = false;
-  //       },
-  //     });
-  // }
+  toggleMixedCaseLetters() {
+    this.isAbleMixedCaseLetters = !this.isAbleMixedCaseLetters;
+  }
 
-  // searchUsers(serchedTerm: string) {
-  //   this.shared
-  //     .getSearchedUsers(serchedTerm, this.selectedPage, this.selectedPageSize)
-  //     .subscribe({
-  //       next: (response) => {
-  //         this.userList = response.response;
-  //         this.totalDataCount = response.rowCount;
+  validateInput(event: any): void {
+    const inputValue = event.target.value;
+    const minValue = this.passwordMinLengthDefault;
+    const maxValue = this.passwordMaxLength || this.passwordMaxLengthDefault;
 
-  //         if (this.totalDataCount > 0) {
-  //           this.updateTable();
-  //           this.loadingInProgress = false;
-  //         } else {
-  //           this.alertService.warningSweetAlertMessage(
-  //             this.appService.popUpMessageConfig[0].NoDataNotificationMessage,
-  //             "No Data!",
-  //             4000
-  //           );
-  //           this.getAllUsers();
-  //         }
-  //       },
-  //       error: (error) => {
-  //         this.alertService.sideErrorAlert(
-  //           "Error",
-  //           this.appService.popUpMessageConfig[0]
-  //             .GetUserListSuccessSideAlertMessage
-  //         );
-
-  //         this.userList = [];
-  //         this.totalDataCount = 0;
-
-  //         this.updateTable();
-  //         this.loadingInProgress = false;
-  //       },
-  //     });
-  // }
-
-  // searchUsersByRole(serchedTerm: string, role: string) {
-  //   this.shared
-  //     .getSearchedUsersByRole(
-  //       serchedTerm,
-  //       role,
-  //       this.selectedPage,
-  //       this.selectedPageSize
-  //     )
-  //     .subscribe({
-  //       next: (response) => {
-  //         this.userList = response.response;
-  //         this.totalDataCount = response.rowCount;
-  //         if (this.totalDataCount > 0) {
-  //           this.updateTable();
-  //           this.loadingInProgress = false;
-  //         } else {
-  //           this.alertService.warningSweetAlertMessage(
-  //             this.appService.popUpMessageConfig[0].NoDataNotificationMessage,
-  //             "No Data!",
-  //             4000
-  //           );
-  //           this.getAllUsers();
-  //         }
-  //       },
-  //       error: (error) => {
-  //         this.alertService.sideErrorAlert(
-  //           "Error",
-  //           this.appService.popUpMessageConfig[0]
-  //             .GetUserListSuccessSideAlertMessage
-  //         );
-
-  //         this.userList = [];
-  //         this.totalDataCount = 0;
-
-  //         this.updateTable();
-  //         this.loadingInProgress = false;
-  //       },
-  //     });
-  // }
+    if (inputValue < minValue) {
+      this.passwordMinLength = minValue;
+    } else if (inputValue > maxValue) {
+      this.passwordMinLength = maxValue;
+    } else {
+      this.passwordMinLength = inputValue;
+    }
+    //have to develop this method
+  }
 }
