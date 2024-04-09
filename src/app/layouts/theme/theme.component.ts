2ff99f5b0468b8ab9e036d56d0afa8e5abad28a2
Thushara Makuloluwa <139602736+ThusharaMakuloluwa@users.noreply.github.com>
2024-04-09 11:33:47 +0530

    Trial Mode background added

--- src/app/layouts/theme/theme.component.ts
+++ src/app/layouts/theme/theme.component.ts
@@ -1,36 +1,43 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { AppService } from 'src/app/app.service';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NavbarComponent } from "../navbar/navbar.component";
+import { AppService } from "src/app/app.service";
+import { AuthService } from "src/app/auth/auth.service";
 
 @Component({
-  selector: 'app-theme',
-  templateUrl: './theme.component.html',
-  styleUrls: ['./theme.component.scss']
+  selector: "app-theme",
+  templateUrl: "./theme.component.html",
+  styleUrls: ["./theme.component.scss"],
 })
 
 /**
  * Horizontal Component
  */
 export class HorizontalComponent implements OnInit {
-
   // colorTheme:string="light";
   showTopbar: boolean = true;
 
-  constructor(private appService: AppService) { }
+  constructor(private appService: AppService, private auth: AuthService) {}
 
   @ViewChild(NavbarComponent) childHorizontalTopBar!: NavbarComponent;
 
+  user = this.auth.getUser();
+  licenseType = "trial"; //this.user?.license
+  activateWatermark: boolean = true;
+
   ngOnInit(): void {
+    if (this.licenseType == "demo" || this.licenseType == "full") {
+      this.activateWatermark = false;
+    }
 
-    document.documentElement.setAttribute('data-layout', 'horizontal');
-    document.documentElement.setAttribute('data-topbar', 'light');
-    document.documentElement.setAttribute('data-sidebar', 'dark');
-    document.documentElement.setAttribute('data-sidebar-size', 'lg');
-    document.documentElement.setAttribute('data-layout-style', 'default');
+    document.documentElement.setAttribute("data-layout", "horizontal");
+    document.documentElement.setAttribute("data-topbar", "light");
+    document.documentElement.setAttribute("data-sidebar", "dark");
+    document.documentElement.setAttribute("data-sidebar-size", "lg");
+    document.documentElement.setAttribute("data-layout-style", "default");
 
-    document.documentElement.setAttribute('data-layout-width', 'fluid');
-    document.documentElement.setAttribute('data-layout-position', 'fixed');
-    document.documentElement.setAttribute('data-preloader', 'disable');
+    document.documentElement.setAttribute("data-layout-width", "fluid");
+    document.documentElement.setAttribute("data-layout-position", "fixed");
+    document.documentElement.setAttribute("data-preloader", "disable");
   }
 
   changeMenu(module: any) {
@@ -42,8 +49,7 @@ export class HorizontalComponent implements OnInit {
    */
   onToggleMobileMenu() {
     if (document.documentElement.clientWidth <= 1024) {
-      document.body.classList.toggle('menu');
+      document.body.classList.toggle("menu");
     }
   }
-
 }
